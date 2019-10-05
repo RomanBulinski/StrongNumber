30ecc0fcbc6b8225234712f58aac680ae5e7a077
roman <aboandrus@gmail.com>
2019-10-05 08:23:46 +0200

    Implemented code

--- src/main/java/StrongNumber.java
+++ src/main/java/StrongNumber.java
@@ -1,7 +1,35 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collector;
+
+import static java.util.stream.Collectors.toList;
+
 public class StrongNumber {
 
+
     public static String isStrongNumber(int num) {
-        return "";
+
+        List<Object> list = Arrays.stream(String.valueOf(num)
+                .split(""))
+                .mapToInt(n ->silnia( n ))
+                .mapToObj(m->String.valueOf(m))
+                .collect(toList());
+        String effect = "";
+        for(int i =0; i <list.size(); i++){
+            effect+=list.get(i);
+        }
+        if( num == Integer.valueOf(effect) ){
+            return "STRONG!!!!";
+        }
+        return "Not Strong !!";
+    }
+
+    static int silnia(String i) {
+        int res = 0;
+        for (int j = 1; j <= Integer.valueOf(i); j++) {
+            res = res * j;
+        }
+        return res;
     }
 
 
--- src/test/java/TestStrongNumber.java
+++ src/test/java/TestStrongNumber.java
@@ -8,8 +8,14 @@ public class TestStrongNumber {
     private static String NOT_STRONG = "Not Strong !!";
 
     @Test
-    public void testSomething() {
+    public void testSomething1() {
         assertEquals(STRONG, StrongNumber.isStrongNumber(1));
+
+    }
+
+    @Test
+    public void testSomething2() {
+
         assertEquals(STRONG, StrongNumber.isStrongNumber(2));
         assertEquals(STRONG, StrongNumber.isStrongNumber(145));
         assertEquals(NOT_STRONG, StrongNumber.isStrongNumber(7));
