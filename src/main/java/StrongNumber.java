1ba2cc5fced3895906e20decacc520d0cb51e775
roman <aboandrus@gmail.com>
2019-10-05 09:14:16 +0200

    Refactor code

--- src/main/java/StrongNumber.java
+++ src/main/java/StrongNumber.java
@@ -1,18 +1,9 @@
 import java.util.Arrays;
-import java.util.List;
-import java.util.function.ToIntFunction;
-import java.util.stream.Collector;
-
-import static java.util.stream.Collectors.toList;
 
 public class StrongNumber {
 
-
     public static String isStrongNumber(int num) {
-
-        int effect = Arrays.stream(String.valueOf(num)
-                .split(""))
-                .mapToInt(n ->silnia( n )).sum();
+        int effect = Arrays.stream(String.valueOf(num).split("")).mapToInt(n ->silnia( n )).sum();
         return ( num == effect ) ? "STRONG!!!!":"Not Strong !!";
     }
 
@@ -23,20 +14,4 @@ public class StrongNumber {
         }
         return res;
     }
-
-    public static String isStrongNumber2(int num) {
-
-        List<Object> list = Arrays.stream(String.valueOf(num)
-                .split(""))
-                .mapToInt(n ->silnia( n ))
-                .mapToObj(m->String.valueOf(m))
-                .collect(toList());
-        int effect = 0;
-        for(int i =0; i <list.size(); i++){
-            effect+= Integer.parseInt( (String)list.get(i) );
-        }
-        return ( num == effect ) ? "STRONG!!!!":"Not Strong !!";
-    }
-
-
 }
